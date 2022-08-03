fc3cdec117989b80a6ac3e49a2c848d555e39ac6
Krzysztof Bieganski <kbieganski@antmicro.com>
2022-08-03 08:57:16 +0200

    Modernize the main file
    Signed-off-by: Krzysztof Bieganski <kbieganski@antmicro.com>

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,15 @@
 #include <Vtop.h>
 #define MAX_TIME 1000
 
-uint64_t main_time = 0;
-double sc_time_stamp() { return main_time; }
-
 int main(int argc, char *argv[]) {
-    Verilated::commandArgs(argc, argv);
     Verilated::debug(0);
+    Verilated::commandArgs(argc, argv);
     Vtop *top = new Vtop;
     while (!Verilated::gotFinish()) {
         top->eval();
         top->clk = 1 & ~top->clk;
-        main_time++;
-        if (main_time > MAX_TIME) {
+        top->contextp()->timeInc(1);
+        if (top->contextp()->time() > MAX_TIME) {
             VL_WRITEF("Test timed out!");
             return -15;
         }
