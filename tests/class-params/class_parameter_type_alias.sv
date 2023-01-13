e7af4fdef44d4c516261d96cb6a14d7d0847b58d
Ryszard Rozak <rrozak@antmicro.com>
2023-01-13 12:57:01 +0100

    Add tests with type aliases
    Signed-off-by: Ryszard Rozak <rrozak@antmicro.com>

---
+++ tests/class-params/class_parameter_type_alias.sv
@@ -0,0 +1,29 @@
+/* *** Issue description
+
+   Parameterized classes are not supported.
+
+   *** End of description
+*/
+// Example
+class SelfRefClassTypeParam #(type T=logic);
+   typedef SelfRefClassTypeParam #(int) self_int_t;
+   T field;
+endclass
+// Example end
+
+
+module top (
+    input clk,
+    output logic [7:0] o
+);
+    SelfRefClassTypeParam src_logic;
+    SelfRefClassTypeParam::self_int_t src_int;
+    initial begin
+        src_int = new;
+        src_logic = new;
+        if ($bits(src_logic.field) != 1) $stop;
+        if ($bits(src_int.field) != 32) $stop;
+        $finish;
+    end
+
+endmodule
