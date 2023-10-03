5f0d5d4e45c75b644c26c2a2ba196e17e7adf7e5
Ryszard Rozak <rrozak@antmicro.com>
2023-10-03 13:47:41 +0200

    Remove tests with random stability and some tests of classes
    Signed-off-by: Ryszard Rozak <rrozak@antmicro.com>

--- tests/uvm-tests/top.sv
+++
@@ -1,8 +0,0 @@
-/*
-:name: easyUVM
-:description: UVM testbench test
-:files: /home/rrozak/antmicro/sv-tests/third_party/tests/easyUVM/top.sv
-:incdirs: /home/rrozak/antmicro/sv-tests/third_party/tests/easyUVM
-:tags: uvm
-:timeout: 100
-*/
