6cac3e6cab3048c293fa04b710a56306235c110d
Ugur Nezir <ugurnezir@outlook.com>
2023-12-29 15:35:05 +0300

    Update script to support multiple branch args

--- scripts/build_and_run.sh
+++ scripts/build_and_run.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-# Define the log file
-LOGFILE="../build_and_run.log"
-
-# Function to execute a command and log its output
-execute_and_log() {
-    echo "Executing: $1"
-    echo "--------------------------------------------------" >> "$LOGFILE"
-    echo "COMMAND: $1" >> "$LOGFILE"
-    echo "OUTPUT:" >> "$LOGFILE"
-    eval $1 >> "$LOGFILE" 2>&1
-    echo "--------------------------------------------------" >> "$LOGFILE"
-}
-
 # List of commands to execute
 echo "Starting build & run script..."
 cd ..
@@ -42,7 +29,7 @@ make -j$(nproc)
 
 echo "Generating and running tests..."
 cd ..
-./gen-tests
+./gen_tests "$@"
 robot *.robot
 
-echo "Script execution completed. Check the log at $LOGFILE"
+echo "Script execution completed."
