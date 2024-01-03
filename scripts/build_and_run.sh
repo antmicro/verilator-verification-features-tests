ee3517e67e808bd19e70d9e29ca9a3bec3f32fe4
yesilzeytin <ugurnezir@outlook.com>
2024-01-03 09:32:37 +0300

    Stable baseline script.

--- scripts/build_and_run.sh
+++ scripts/build_and_run.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+# Define the log file
+LOGFILE="../build_and_run.log"
+
+# Function to execute a command and log its output
+execute_and_log() {
+    echo "Executing: $1"
+    echo "--------------------------------------------------" >> "$LOGFILE"
+    echo "COMMAND: $1" >> "$LOGFILE"
+    echo "OUTPUT:" >> "$LOGFILE"
+    eval $1 >> "$LOGFILE" 2>&1
+    echo "--------------------------------------------------" >> "$LOGFILE"
+}
+
 # List of commands to execute
 echo "Starting build & run script..."
+git submodule update --init --recursive
 cd ..
 pwd
 
@@ -20,16 +34,10 @@ autoconf
 ./configure
 make -j$(nproc)
 
-echo "Building uvm-verilator..."
-cd ../../uvm
-git show -q
-autoconf
-./configure
-make -j$(nproc)
-
 echo "Generating and running tests..."
-cd ..
-./gen_tests "$@"
+cd ../..
+pwd
+./gen-tests "$@"
 robot *.robot
 
 echo "Script execution completed."
