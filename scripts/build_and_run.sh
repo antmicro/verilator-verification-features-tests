df991991859e29f8dee0eea705a7231b56d9498d
yesilzeytin <ugurnezir@outlook.com>
2024-01-03 10:15:33 +0300

    Latest stable run script

--- scripts/build_and_run.sh
+++ scripts/build_and_run.sh
@@ -1,43 +1,54 @@
 #!/bin/bash
 
-# Define the log file
-LOGFILE="../build_and_run.log"
+# Determine the directory where the script is located
+SCRIPT_DIR=$(realpath $(dirname "$0"))
+
+# Define the log file relative to the script's location
+LOGFILE="$SCRIPT_DIR/../build_and_run.log"
+
+# Clear the log file at the start of the script
+> "$LOGFILE"
 
 # Function to execute a command and log its output
 execute_and_log() {
-    echo "Executing: $1"
+    COMMAND="$1"
+    shift
+    ARGS="$@"
+    echo "Executing: $COMMAND $ARGS"
     echo "--------------------------------------------------" >> "$LOGFILE"
-    echo "COMMAND: $1" >> "$LOGFILE"
+    echo "COMMAND: $COMMAND $ARGS" >> "$LOGFILE"
     echo "OUTPUT:" >> "$LOGFILE"
-    eval $1 >> "$LOGFILE" 2>&1
+    eval "$COMMAND $ARGS" >> "$LOGFILE" 2>&1
     echo "--------------------------------------------------" >> "$LOGFILE"
 }
 
-# List of commands to execute
+# Start of the script execution
 echo "Starting build & run script..."
-git submodule update --init --recursive
-cd ..
-pwd
+execute_and_log "git submodule update --init --recursive"
+
+# Adjust the directory changes to be relative to the script's location
+execute_and_log "cd $SCRIPT_DIR/.."
+execute_and_log "pwd"
 
 echo "Building verilator/master..."
-cd verilator/master
-pwd
-git show -q
-autoconf
-./configure
-make -j$(nproc)
+execute_and_log "cd $SCRIPT_DIR/../verilator/master"
+execute_and_log "pwd"
+execute_and_log "git show -q"
+execute_and_log "autoconf"
+execute_and_log "./configure"
+execute_and_log "make -j$(nproc)"
 
 echo "Building verilator/randomize-constraints..."
-cd ../randomize-constraints
-git show -q
-autoconf
-./configure
-make -j$(nproc)
+execute_and_log "cd $SCRIPT_DIR/../verilator/randomize-constraints"
+execute_and_log "git show -q"
+execute_and_log "autoconf"
+execute_and_log "./configure"
+execute_and_log "make -j$(nproc)"
 
 echo "Generating and running tests..."
-cd ../..
-pwd
-./gen-tests "$@"
-robot *.robot
+execute_and_log "cd $SCRIPT_DIR/../"
+execute_and_log "pwd"
+execute_and_log "./gen-tests $@"
+execute_and_log "robot $SCRIPT_DIR/../*.robot"
 
 echo "Script execution completed."
