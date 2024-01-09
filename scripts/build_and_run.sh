50e1942fffbd2fc6fe9f70de1c0ce385603d0d1f
Ugur Nezir <ugurnezir@outlook.com>
2024-01-09 09:33:20 +0300

    Update build_and_run.sh based on reviewer suggestion

--- scripts/build_and_run.sh
+++ scripts/build_and_run.sh
@@ -2,9 +2,10 @@
 
 # Determine the directory where the script is located
 SCRIPT_DIR=$(realpath $(dirname "$0"))
+MAIN_DIR=$SCRIPT_DIR/..
 
 # Define the log file relative to the script's location
-LOGFILE="$SCRIPT_DIR/../build_and_run.log"
+LOGFILE="$MAIN_DIR/build_and_run.log"
 
 # Clear the log file at the start of the script
 > "$LOGFILE"
@@ -27,7 +28,7 @@ echo "Starting build & run script..."
 execute_and_log "git submodule update --init --recursive"
 
 echo "Building verilator/master..."
-execute_and_log "cd $SCRIPT_DIR/../verilator/master"
+execute_and_log "cd $MAIN_DIR/verilator/master"
 execute_and_log "pwd"
 execute_and_log "git show -q"
 execute_and_log "autoconf"
@@ -35,16 +36,16 @@ execute_and_log "./configure"
 execute_and_log "make -j$(nproc)"
 
 echo "Building verilator/randomize-constraints..."
-execute_and_log "cd $SCRIPT_DIR/../verilator/randomize-constraints"
+execute_and_log "cd $MAIN_DIR/verilator/randomize-constraints"
 execute_and_log "git show -q"
 execute_and_log "autoconf"
 execute_and_log "./configure"
 execute_and_log "make -j$(nproc)"
 
 echo "Generating and running tests..."
-execute_and_log "cd $SCRIPT_DIR/../"
+execute_and_log "cd $MAIN_DIR/"
 execute_and_log "pwd"
 execute_and_log "./gen-tests $@"
-execute_and_log "robot $SCRIPT_DIR/../*.robot"
+execute_and_log "robot $MAIN_DIR/*.robot"
 
 echo "Script execution completed."
